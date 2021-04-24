5e028b24d1c6b94c06afbd57d89b7b39cfde4e55
podiumdesu <wwhmomo@gmail.com>
2021-04-24 15:27:04 +0800

    Site updated: 2021-04-24 15:27:03

--- js/src/utils.js
+++ js/src/utils.js
@@ -10,6 +10,9 @@ NexT.utils = NexT.$u = {
       .not('.group-picture img, .post-gallery img')
       .each(function () {
         var $image = $(this);
+        if ($(this).hasClass('nofancybox')) return;
+          var imageTitle = $image.attr('title');
+          var $imageWrapLink = $image.parent('a');
         var imageTitle = $image.attr('title');
         var $imageWrapLink = $image.parent('a');
 
